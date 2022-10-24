603ca973b6fa9bd0b9c6006cc6d804af74bfbc85
peidongxie <32510900+peidongxie@users.noreply.github.com>
2022-10-24 16:10:26 +0000

    Deploying to gh-pages from @ peidongxie/south-wind@e3d234409189d938043a9f7a836324abd8f87ba3 🚀

--- assets/index.html.1e95d695.js
+++ assets/index.html.1e95d695.js
@@ -1,1 +1,1 @@
-const e=JSON.parse('{"key":"v-7e978520","path":"/courses/","title":"\u8BFE\u7A0B\u5217\u8868","lang":"zh-CN","frontmatter":{},"excerpt":"","headers":[],"git":{"updatedTime":1630288520000},"filePathRelative":"courses/README.md"}');export{e as data};
+const e=JSON.parse('{"key":"v-7e978520","path":"/courses/","title":"课程列表","lang":"zh-CN","frontmatter":{},"excerpt":"","headers":[],"git":{"updatedTime":1630288520000},"filePathRelative":"courses/README.md"}');export{e as data};
